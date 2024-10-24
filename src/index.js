40622d7ecf6bf3ee01b27925ce004b45113ae051
Ayush Verma <ayushverma2112004@gmail.com>
2024-10-24 21:49:15 +0530

    commiting files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 // require('dotenv').config({path:'./env'})
+import { app } from './app.js';
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
 dotenv.config({
     path:'./env'
 });
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 3000,()=>{
+        console.log(`server is running at port ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB coneection FAILED !!",err)
+})
---
+++ src/utils/asynHandler.js
@@ -0,0 +1,12 @@
+const asyncHandler = (reqHandler) => async(req,res,next) => {
+    try {
+        await reqHandler(req,res,next)
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+
+export {asyncHandler}
